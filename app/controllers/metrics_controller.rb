65b2eb35b0aa3ebeac6482ba8751355539db2486
Kim Daekwon <propellerheaven@gmail.com>
2013-08-13 05:06:28 +0900

    added metrics controllers

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,4 +1,36 @@
 class MetricsController < ApplicationController
+  def synopsis
+  end
+
+  def files
+  end
+
   def classes
   end
+
+  def methods
+  end
+  
+  def churn
+    
+  end
+
+  def complexity
+    
+  end
+
+  def duplicity
+    
+  end
+
+  def smells
+    
+  end
+
+  def report
+  end
+
+  def timeline
+  end
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,6 +61,9 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def settings
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
