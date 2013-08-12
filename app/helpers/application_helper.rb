89ebf99d8d1ccd375d00ce9e5ccb21c59478568c
Kim Daekwon <propellerheaven@gmail.com>
2013-08-12 16:30:02 +0900

    add class login form in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,37 @@ module ApplicationHelper
     @resource ||= User.new
   end
 
+  def login_form_for_header
+    simple_form_for resource, :as => resource_name, :url => session_path(resource_name), html: { class: "navbar-form pull-right"} do |f|
+      email_input(f) + " " + password_input(f) + " " + signin_submit(f)
+    end
+  end
 
+  def email_input(form)
+    form.input(:email,
+               wrapper: false,
+               label: false,
+               input_html: {
+                 class: "form-control",
+                 style: "width: 200px;",
+                 placeholder: "ID",
+                 value: "admin@stub.lapisan"})
+  end
+
+  def password_input(form)
+    form.input(:password,
+               wrapper: false,
+               label: false,
+               input_html: {
+                 class: "form-control",
+                 style: "width: 200px;",
+                 placeholder: "Password",
+                 value: "abcd1234"})
+  end
+
+  def signin_submit(form)
+    form.submit("Sign in",
+                class: "btn btn-default navbar-btn",
+                style: "margin-top: -3px;")
+  end
 end
