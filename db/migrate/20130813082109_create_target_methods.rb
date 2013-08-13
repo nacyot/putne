001218488ae8efccf80b1e765faab7b1dda79108
Kim Daekwon <propellerheaven@gmail.com>
2013-08-13 17:44:54 +0900

    add fullname to target_method

--- db/migrate/20130813082109_create_target_methods.rb
+++ db/migrate/20130813082109_create_target_methods.rb
@@ -2,6 +2,7 @@ class CreateTargetMethods < ActiveRecord::Migration
   def change
     create_table :target_methods do |t|
       t.string :name
+      t.string :full_name
       t.integer :report_id
       t.integer :class_id
 
