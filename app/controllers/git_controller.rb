b1e8c16b0bb0dca16f5b449f11a1b60e940b57b2
Kim Daekwon <propellerheaven@gmail.com>
2013-08-19 16:54:54 +0900

    add pagination to commits and rm branches sidebar

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
 class GitController < ApplicationController
   load_and_authorize_resource class: Project
   
   def branches
     @project = Project.find params[:project_id]
+    @report = Report.find params[:report_id]
   end
 
   def commits
     @project = Project.find params[:project_id]
     @report = Report.find params[:report_id]
-    @repository = @project.repository   
+    @commits = @project.repository.commits.page(params[:page]).per(10)
   end
 end
