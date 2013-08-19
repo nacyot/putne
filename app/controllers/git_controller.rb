e6f4268abc9624cced42c33f7152467e69e0ec2f
Kim Daekwon <propellerheaven@gmail.com>
2013-08-19 18:06:03 +0900

    add page title

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -3,11 +3,13 @@ class GitController < ApplicationController
   load_and_authorize_resource class: Project
   
   def branches
+    @title = "Branches"
     @project = Project.find params[:project_id]
     @report = Report.find params[:report_id]
   end
 
   def commits
+    @title = "Commits" 
     @project = Project.find params[:project_id]
     @report = Report.find params[:report_id]
     @commits = @project.repository.commits.page(params[:page]).per(10)
--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -4,6 +4,7 @@ class MetricsController < ApplicationController
   load_and_authorize_resource class: Project
 
   def files
+    @title = "Files"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
     @files = @report.target_files.page(params[:page]).per(25)
@@ -15,9 +16,12 @@ class MetricsController < ApplicationController
     
     file_name = TargetFile.find(params[:file_id]).path
     @file = (@project.repository.git.last_commit.tree / file_name).data
+
+    @title = "File viewer - #{file_name}"
   end
   
   def classes
+    @title = "Classes"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
     @classes = @report.target_classes.page(params[:page]).per(25)
@@ -29,9 +33,11 @@ class MetricsController < ApplicationController
 
     file_name = TargetClass.find(params[:class_id]).target_file.path
     @file = (@project.repository.git.last_commit.tree / file_name).data
+    @title = "Class viewer - #{file_name}"
   end
   
   def methods
+    @title = "Methods"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
     @methods = @report.target_methods.page(params[:page]).per(25)
@@ -55,35 +61,42 @@ class MetricsController < ApplicationController
     @method = ""
     @method_name = method_name
     find_source_code file, method_name
+
+    @title = "Method vierew - #{@method_name}"
   end
   
   def churn
+    @title = "Churns"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
   end
 
   def complexity
+    @title = "Complexities"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
     @methods = @report.target_methods.page(params[:page]).per(25)
   end
 
   def duplicity
+    @title = "Duplications"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
   end
 
   def smells
+    @title = "Smells"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
   end
 
   def timeline
+    @titmle = "Timeline"
     @project = Project.find(params[:project_id])
     @report = Report.find(params[:report_id])
   end
 
-
+  private
   def find_source_code(file, method_name)
     ast_root = Parser::CurrentRuby.parse(file)
     recursive_search_ast(ast_root, method_name)
