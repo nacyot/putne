5237f8ae165a33d505dafe221cf4a85fa69814b6
Kim Daekwon <propellerheaven@gmail.com>
2013-09-19 15:38:29 +0900

    Creating lock while code metrics running

--- app/workers/init_repository_worker.rb
+++ app/workers/init_repository_worker.rb
@@ -4,8 +4,15 @@ class InitRepositoryWorker
   
   def perform(repo_id)
     repo = Repository.find(repo_id)
+
+    puts "create .metrics_lock"
+    `touch #{File.join(repo.workspace_path, ".git", ".metrics_lock")}`
+
     repo.init_repository
     repo.create_recent_report
+
+    puts "delete .metrics_lock"
+    `rm #{File.join(repo.workspace_path, ".git", ".metrics_lock")}`
   end
 end
 
--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -4,8 +4,28 @@ class ReportWorker
   
   def perform(repo_id, hash)
     repo = Repository.find repo_id
-    repo.create_report hash
+    lock = File.join(repo.workspace_path, ".git", ".metrics_lock")
+
+    0.upto(30) do
+      unless File.exists?(lock)
+        puts "create .metrics_lock"
+        `touch #{lock}`
+
+        repo.create_report hash
+
+        puts "delete .metrics_lock"
+        `rm #{lock}`
+        break
+      else
+        puts "This repository locked. #{repo.workspace_path}"
+        sleep 5
+      end
+    end
+  ensure
+    puts "delete .metrics_lock"
+    `rm #{lock}`
   end
+  
 end
 
 
