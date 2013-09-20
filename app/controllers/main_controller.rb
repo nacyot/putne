5bd7f09e147cae3aca992073d36d98d2c2928039
Kim Daekwon <propellerheaven@gmail.com>
2013-09-21 01:35:36 +0900

    Refine design of dashboard page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
-    @projects = current_user.projects.order("id desc") if signed_in?
-    @commits = current_user.commits.page(params[:page]).per(10) if signed_in?
+    @projects = current_user.projects.order("id desc").page(params[:page]) if signed_in?
+    # @commits = current_user.commits.page(params[:page]).per(10) if signed_in?
   end
 
   def about
