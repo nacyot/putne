f8e2923e3a5519ab41bf67f9448bb8f33e3a53ee
Kim Daekwon <propellerheaven@gmail.com>
2013-09-24 20:24:41 +0900

    fix db migration down process

--- db/migrate/20130924085827_refine_score_and_smell.rb
+++ db/migrate/20130924085827_refine_score_and_smell.rb
@@ -1,9 +1,16 @@
 class RefineScoreAndSmell < ActiveRecord::Migration
   def change
-    remove_column :scores, :smell_category_id
     add_column :scores, :score_category_id, :integer
     add_column :scores, :score_soucre_id, :integer
     add_column :smells, :smell_category_id, :integer
     add_column :smells, :smell_soucre_id, :integer
+  end
+  
+  def up
+    remove_column :scores, :smell_category_id
+  end
+
+  def down
+    add_column :scores, :smell_category_id, :integer
   end
 end
