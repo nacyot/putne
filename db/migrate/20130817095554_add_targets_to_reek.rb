19ab5914f93406206526fe16f41a917e9b298824
Kim Daekwon <propellerheaven@gmail.com>
2013-09-24 16:34:09 +0900

    Delete migration files related with old score model

--- db/migrate/20130817095554_add_targets_to_reek.rb
+++
@@ -1,6 +0,0 @@
-class AddTargetsToReek < ActiveRecord::Migration
-  def change
-    add_column :reek_smells, :target_file_id, :integer
-    add_column :reek_smells, :target_class_id, :integer
-  end
-end
