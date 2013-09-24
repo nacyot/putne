b129c8deb7554fe7b1eb6b21fabe90dc8b52f771
Kim Daekwon <propellerheaven@gmail.com>
2013-09-24 15:28:32 +0900

    Fix targetable varbiable to symbol

--- app/models/target_file.rb
+++ app/models/target_file.rb
@@ -1,15 +1,10 @@
 class TargetFile < ActiveRecord::Base
   belongs_to :report
-  has_one :churn, as: :targetable
 
-  has_many :scores, as: targetable
-  has_many :smells, as: targetable
-  
-  # decapreted
-  has_many :roodi
   has_many :target_classes
-  has_many :reek_smells
-
+  has_many :scores, as: :targetable
+  has_many :smells, as: :targetable
+  
   validates :path, :uniqueness => {
     :scope => :report_id,
     :message => 'cannot have two category with same path and report_id'}
--- app/models/target_method.rb
+++ app/models/target_method.rb
@@ -2,15 +2,9 @@ class TargetMethod < ActiveRecord::Base
   belongs_to :report
   belongs_to :target_class
 
-  has_many :scores, as: targetable
-  has_many :smells, as: targetable
+  has_many :scores, as: :targetable
+  has_many :smells, as: :targetable
   
-  # decapreted!
-  has_one :churn, as: :targetable
-  has_one :complexity_score
-  has_many :reek_smells
-
   validates_presence_of :report, :target_class, :name
   validates :name, :format => { with: /[#:]/ } 
-
 end
