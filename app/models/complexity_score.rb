c2d06c9749b32bc864187230e9fa28eb1fcf2dc8
Kim Daekwon <propellerheaven@gmail.com>
2013-09-24 13:00:20 +0900

    rm decapreted score model files

--- app/models/complexity_score.rb
+++
@@ -1,4 +0,0 @@
-class ComplexityScore < ActiveRecord::Base
-  belongs_to :report
-  belongs_to :target_method
-end
--- app/models/roodi.rb
+++
@@ -1,4 +0,0 @@
-class Roodi < ActiveRecord::Base
-  belongs_to :report
-  has_one :file_line_info, as: :lineable
-end
