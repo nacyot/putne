34a08239493ca92193e048c245fec01ae5f09132
Kim Daekwon <propellerheaven@gmail.com>
2013-09-24 17:44:57 +0900

    rm Score Category model

--- app/models/smell_source.rb
+++
@@ -1,2 +0,0 @@
-class SmellSource < ActiveRecord::Base
-end
--- db/migrate/20130924084157_create_smell_sources.rb
+++
@@ -1,9 +0,0 @@
-class CreateSmellSources < ActiveRecord::Migration
-  def change
-    create_table :smell_sources do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
