56619d44c3248712dd1c50db72a1dae25da5b531
Kim Daekwon <propellerheaven@gmail.com>
2013-09-25 10:18:11 +0900

    Refine project delete button #87

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -80,7 +80,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to root_url }
       format.json { head :no_content }
     end
   end
