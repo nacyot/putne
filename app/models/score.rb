f5cb15b6c7fefb1554a3ada926937d2824ec8c7b
Kim Daekwon <propellerheaven@gmail.com>
2013-09-25 16:13:42 +0900

    Add targetable scopes

--- app/models/score.rb
+++ app/models/score.rb
@@ -6,12 +6,19 @@ class Score < ActiveRecord::Base
 
   validates_presence_of :targetable_id, :targetable_type, :report, :score, :score_category, :score_source
 
+  scope :ordered, -> { order "score" }
+  scope :reverse_ordered, -> { order "score desc" }
+
+  scope :type_file, -> { where(targetable_type: "Targetfile") }
+  scope :type_class, -> { where(targetable_type: "TargetClass") }
+  scope :type_method, -> { where(targetable_type: "TargetMethod") }
+
+  scope :churns, -> { where(score_category: ScoreCategory.find_by(name: "CHURN")) }
+  scope :file_churns, -> { where(score_source: ScoreSource.find_by(name: "FILE_CHURN"))  }
+  scope :class_churns, -> { where(score_source: ScoreSource.find_by(name: "CLASS_CHURN"))  }
+  scope :method_churns, -> { where(score_source: ScoreSource.find_by(name: "METHOD_CHURN"))  }
+
   scope :flogs, -> { where(score_category: ScoreCategory.find_by(name: "COMPLEXITY"), score_source: ScoreSource.find_by(name: "FLOG"))  }
   scope :saikuros, -> { where(score_category: ScoreCategory.find_by(name: "COMPLEXITY"), score_source: ScoreSource.find_by(name: "SAIKURO")) }
   scope :locs, -> { where(score_category: ScoreCategory.find_by(name: "LOC"), score_source: ScoreSource.find_by(name: "SAIKURO"))  }
-
-  scope :churns, -> { where(score_category: ScoreCategory.find_by(name: "CHURN")) }
-  scope :file_churns, -> { where(score_category: ScoreCategory.find_by(name: "CHURN"), score_source: ScoreSource.find_by(name: "FILE_CHURN"))  }
-  scope :class_churns, -> { where(score_category: ScoreCategory.find_by(name: "CHURN"), score_source: ScoreSource.find_by(name: "CLASS_CHURN"))  }
-  scope :method_churns, -> { where(score_category: ScoreCategory.find_by(name: "CHURN"), score_source: ScoreSource.find_by(name: "METHOD_CHURN"))  }
 end
