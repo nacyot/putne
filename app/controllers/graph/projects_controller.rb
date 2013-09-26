c20caa2c0ea7d8024eb87db4fdba07b06572f0f1
Kim Daekwon <propellerheaven@gmail.com>
2013-09-26 10:59:16 +0900

    Refine controllers

--- app/controllers/graph/projects_controller.rb
+++ app/controllers/graph/projects_controller.rb
@@ -1,5 +1,5 @@
 class Graph::ProjectsController < ApplicationController
   def sunburst_chart
-    render :json => {name: "Putne", children: Report.first.flog_scores_group_by_path }
+    render :json => {name: "Putne", children: Project.find(params[:project_id]).reports.last.flog_scores_group_by_path }
   end
 end
