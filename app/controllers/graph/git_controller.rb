792faadf7dc04905c29260c108bd6ee570851596
Kim Daekwon <propellerheaven@gmail.com>
2013-09-26 11:24:28 +0900

    Refine project calendar view

--- app/controllers/graph/git_controller.rb
+++ app/controllers/graph/git_controller.rb
@@ -1,10 +1,10 @@
 class Graph::GitController < ApplicationController
   def dates
     text = CSV.generate do |csv|
-      csv << %w(date value)
+      csv << %w(Date Value)
       Git.new(Project.find(params[:project_id]).repository).dates.each { |line| csv << line }
     end
     
-    render text: text
+    render text: text, content_type: 'text/html'
   end
 end
